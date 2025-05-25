1b9bd5116a00767321bf6e7a2ccd5ab0bb60d19f
sowmyayadav0 <sowmyayadav888@gmail.com>
2025-05-25 13:32:07 +0530

    Add .gitignore to exclude node_modules and env files

---
+++ react-job-portal-main/frontend/src/components/Layout/Footer.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import {Context} from "../../main"
+import {Link} from "react-router-dom"
+import { FaGithub , FaLinkedin} from "react-icons/fa"
+import { SiLeetcode } from "react-icons/si";
+import { RiInstagramFill} from "react-icons/ri"
+function Footer() {
+  const {isAuthorized}  = useContext(Context)
+  return (
+    <footer className= {isAuthorized ? "footerShow" : "footerHide"}>
+<div>&copy; All Rights Reserved by Sowmya.</div>
+<div>
+  <Link to={'https://github.com/sowmyayadav0'} target='github'><FaGithub></FaGithub></Link>
+  <Link to={'https://leetcode.com/u/koradasowmya/'} target='leetcode'><SiLeetcode></SiLeetcode></Link>
+  <Link to={'https://www.linkedin.com/in/sowmya-yadav-36b45a276/'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
+</div>
+      
+    </footer>
+  )
+}
+
+export default Footer
